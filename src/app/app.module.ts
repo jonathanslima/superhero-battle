ab4706f600754fb535c0552fdfcfb1ba504b802e
Lima <jonathan.da.s.lima@accenture.com>
2020-03-09 23:36:00 -0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OpeningComponent } from './opening/opening.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpeningComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
