cb730a5e26e4110da608eb1cc610ab6d85fb38bf
Lima <jonathan.da.s.lima@accenture.com>
2020-03-11 15:21:03 -0300

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OpeningComponent } from './opening/opening.component';
 import { MessagesComponent } from './messages/messages.component';
+import { CardComponent } from './card/card.component';
+import { BattleBoxComponent } from './battle-box/battle-box.component';
+import { ButtonActionComponent } from './button-action/button-action.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OpeningComponent,
-    MessagesComponent
+    MessagesComponent,
+    CardComponent,
+    BattleBoxComponent,
+    ButtonActionComponent
   ],
   imports: [
     BrowserModule,
