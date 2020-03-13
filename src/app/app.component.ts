3ec831a82869ad1f75a209ffb10c550faad9bda1
Lima <jonathan.da.s.lima@accenture.com>
2020-03-12 21:49:24 -0300

    add data in card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { retrieveData } from './services/herodata.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [retrieveData]
 })
-export class AppComponent {
-  title = 'superhero-battle';
+export class AppComponent implements OnInit{
+  public title:string = 'superhero-battle';
+  public players: Array<object> = [];
+
+  constructor(private retrieveData: retrieveData){}
+
+  ngOnInit() {
+    let p1Random = Math.floor(Math.random() * 731) + 1,
+        p2Random = Math.floor(Math.random() * 731) + 1;
+
+    this.retrieveData.getHeroInfo(p1Random).then(hero => { this.players.push(hero) })
+    this.retrieveData.getHeroInfo(p2Random).then(hero => { this.players.push(hero) })
+  }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"]
 })
 export class CardComponent implements OnInit {
+  @Input() public players: Array<any>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  checkLifeInPct(int, alt) {
+    let life = int,
+      total = alt,
+      pct;
+
+    if (life == total) return 100;
+    else {
+      pct = (life * 100) / total;
+      return pct.toFixed(2);
+    }
   }
 
+  getHeros() {
+    let p1 = this.players[0].powerstats;
+    let p2 = this.players[1].powerstats;
+
+    // add life
+    p1.life =
+      (Number(p1.intelligence == "null" ? 50 : p1.intelligence) +
+        Number(p1.strength == "null" ? 50 : p1.strength) +
+        Number(p1.speed == "null" ? 50 : p1.speed) +
+        Number(p1.durability == "null" ? 50 : p1.durability) +
+        Number(p1.power == "null" ? 50 : p1.power) +
+        Number(p1.combat == "null" ? 50 : p1.combat)) *
+      3;
+
+    p2.life =
+      (Number(p2.intelligence == "null" ? 50 : p2.intelligence) +
+        Number(p2.strength == "null" ? 50 : p2.strength) +
+        Number(p2.speed == "null" ? 50 : p2.speed) +
+        Number(p2.durability == "null" ? 50 : p2.durability) +
+        Number(p2.power == "null" ? 50 : p2.power) +
+        Number(p2.combat == "null" ? 50 : p2.combat)) *
+      3;
+
+    let lifeImutableP1 = p1.life;
+    let lifeImutableP2 = p2.life;
+
+    p1.lifePct = this.checkLifeInPct(p1.life, lifeImutableP1);
+    p2.lifePct = this.checkLifeInPct(p2.life, lifeImutableP2);
+
+    console.log(this.players);
+  }
+
+  ngOnInit() {}
 }
